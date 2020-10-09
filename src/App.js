8c5229f65723125035064d4afa1c5085a4b0b9be
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-08 21:11:16 -0700

    Added Main Features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [yes, setYes] = useState(0);
+  const [no, setNo] = useState(0);
+
+  const add = () => {
+    return setYes(yes + 1);
+  };
+
+  const subtract = () => {
+    return setNo(no + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="body">
+      <h1>You killed an enemy. Did the part drop?</h1>
+      <div className="buttonContainer">
+        <div className="buttonBox">
+          <button onClick={add}>Yes</button>
+          <h3>Total Yes: {yes}</h3>
+        </div>
+        <div className="buttonBox">
+          <button onClick={subtract}>No</button>
+          <h3>Total No: {no}</h3>
+        </div>
+      </div>
+      <h2 className="drop">Current Drop Rate:</h2>
+      <h2>{yes > 0 ? (yes / (yes + no)) * 100 : 0}%</h2>
+      </section>
     </div>
   );
 }
